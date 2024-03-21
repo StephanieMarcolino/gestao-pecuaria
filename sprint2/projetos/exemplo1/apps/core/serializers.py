477afc563c20348a1b4379f40ce94cf72e409897
StephanieMarcolino <stephaniemarcolino@gmail.com>
2024-03-21 16:09:37 -0300

    sprint2

--- sprint2/projetos/exemplo1/apps/core/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from core.models import Produtor
-
-class ProdutorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Produtor
-        fields ="__all__"
- # fields = ("nome", "cpf", "email", "senha") 
